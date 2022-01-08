0816d7f60968b12a0ea6f4762aca1daaf702cc9d
jinho <g1063114@naver.com>
2022-01-08 20:44:44 +0900

    password encrypted

--- src/main/java/com/example/userservicecloud/service/UserServiceImpl.java
+++ src/main/java/com/example/userservicecloud/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.userservicecloud.service;
 import com.example.userservicecloud.dto.UserDto;
 import com.example.userservicecloud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,13 +12,19 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService{
 
-    @Autowired
     UserRepository userRepository;
+    BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
-        userDto.setEncryptedPwd("encrypted");
+        userDto.setEncryptedPwd(passwordEncoder.encode(userDto.getPassword()));
         userDto.setCreatedAt(LocalDateTime.now());
         userRepository.save(userDto.toEntity(userDto));
         return userDto;
